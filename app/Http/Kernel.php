db69b7ccf14bd3ed96f9fbefaa25910e9af3dd09
likun <joollee@qq.com>
2019-02-12 20:00:02 +0800

    添加了自动刷新的登陆验证

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,8 +52,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auto.guards' => \App\Http\Middleware\AutoGuards::class,
-        'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'auth' => \App\Http\Middleware\AutoRefreshAuthenticate::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
