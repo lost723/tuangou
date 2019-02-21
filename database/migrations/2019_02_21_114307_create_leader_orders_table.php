53805638a9f236b3681f2de9e8a96dea40701589
lost <Mr_liuxl@163.com>
2019-02-21 12:11:53 +0000

    添加团长挑货订单表

---
+++ database/migrations/2019_02_21_114307_create_leader_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLeaderOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leader_orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('leaderid')->comment('团长id');
+            $table->unsignedInteger('promotionid')->comment('活动id');
+            $table->unsignedInteger('productid')->comment('商品id');
+            $table->unsignedInteger('num')->comment('订单商品数量');
+            $table->unsignedInteger('expire')->comment('活动截止时间');
+            $table->string('check')->comment('核销码');
+            $table->unsignedTinyInteger('status')->comment('-1:作废 1:进行中 2:配送中 0:已签收 ');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leader_orders');
+    }
+}
