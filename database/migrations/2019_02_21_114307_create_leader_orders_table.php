1b610e66bb8f9c154089955417764c7e36670f03
lost <Mr_liuxl@163.com>
2019-02-22 00:35:04 +0000

    增加团长挑货订单表中字段 sales note

--- database/migrations/2019_02_21_114307_create_leader_orders_table.php
+++ database/migrations/2019_02_21_114307_create_leader_orders_table.php
@@ -18,10 +18,12 @@ class CreateLeaderOrdersTable extends Migration
             $table->unsignedInteger('leaderid')->comment('团长id');
             $table->unsignedInteger('promotionid')->comment('活动id');
             $table->unsignedInteger('productid')->comment('商品id');
-            $table->unsignedInteger('num')->comment('订单商品数量');
+            $table->unsignedInteger('num')->default(0)->comment('挑选该商品数量');
+            $table->unsignedInteger('sales')->default(0)->comment('该商品销量');
             $table->unsignedInteger('expire')->comment('活动截止时间');
             $table->string('check')->comment('核销码');
-            $table->unsignedTinyInteger('status')->comment('-1:作废 1:进行中 2:配送中 0:已签收 ');
+            $table->unsignedTinyInteger('status')->comment('-1:异常 1:进行中 2:配送中 0:已签收 ');
+            $table->string('note')->comment('订单异常备注');
             $table->timestamps();
         });
     }
