a732b75b189be4fd06452382be6f14cc872820a8
likun <joollee@qq.com>
2019-03-15 18:18:11 +0800

    修改了活动

--- database/migrations/2019_02_20_031028_create_promotions_table.php
+++ database/migrations/2019_02_20_031028_create_promotions_table.php
@@ -19,12 +19,14 @@ class CreatePromotionsTable extends Migration
             $table->unsignedBigInteger('optid')->comment('操作人id');
             $table->unsignedBigInteger('productid')->comment('产品id');
             $table->unsignedDecimal('price')->comment('活动售价');
+            $table->unsignedInteger('start')->comment('活动开始时间');
             $table->unsignedInteger('expire')->comment('活动截止时间');
             $table->unsignedInteger('deliveryday')->comment('配送日期，时间戳');
             $table->unsignedInteger('aftersale')->comment('售后截止日期,时间戳');
-            $table->unsignedInteger('stock')->default(0)->comment('库存量，默认是0：预定不关心库存。 确实有库存的，输入真实库存数量');
-            $table->unsignedDecimal('sales')->default(0)->comment('卖出去的数量');
-            $table->unsignedDecimal('leaders')->default(0)->comment('参与的团长数量');
+            $table->unsignedInteger('stock')->default(0)->comment('库存量，stockable=1时有效');
+            $table->unsignedInteger('stockable')->default(0)->comment('需要库存=1； 默认不需要库存=0');
+            $table->unsignedInteger('sales')->default(0)->comment('卖出去的数量');
+            $table->unsignedInteger('leaders')->default(0)->comment('参与的团长数量');
             $table->unsignedTinyInteger('status')->default(0)->comment('0:未发布 1：进行中 2：备货中 3：配送中 4：已签收 9：结束');
             $table->timestamps();
         });
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,10 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $str = "2019-03-15T03:48:07.764Z";
+    $time = strtotime($str);
+    $date = date('Y-m-d H:i:s', $time);
+    var_dump($str);
+    var_dump($time);
+    var_dump($date);
 });
