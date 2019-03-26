6b3359ab86cabd9c7f53fd8b499e1df358dd3f2b
lost <Mr_liuxl@163.com>
2019-03-26 08:47:27 +0000

    监听器绑定事件

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -17,19 +17,32 @@ class EventServiceProvider extends ServiceProvider
             'App\Listeners\UpdateSalesAndStockListener', # 更新库存+销量
             'App\Listeners\AddDelayedTaskListener',      # 添加延迟任务检测订单超时
         ],
-        'App\Events\LeaderCheckEvent' => [
+        'App\Events\LeaderCheckEvent' => [               # 团长签收事件
             'App\Listeners\LeaderCheckListener',
         ],
-        'App\Events\LeaderVerifyEvent' => [
+        'App\Events\LeaderVerifyEvent' => [              # 团长核销事件
             'App\Listeners\LeaderVerifyListener',
+        ],
+        'App\Events\CargosEvent' => [                    # 加入购物车事件
+            'App\Listeners\UpdateCargosCountListener',
+        ],
+        'App\Events\ShareEvent' => [                     # 分享事件
+            'App\Listeners\UpdateShareCountListener',    # 更新分享数量
+        ],
+        'App\Events\ViewEvent' => [                     # 浏览事件
+            'App\Listeners\UpdateViewCountListener',
         ],
         'App\Events\CancelOrderEvent' => [              # 取消订单
             'App\Listeners\UpdateSalesAndStockListener',# 更新库存+销量
         ],
-        'App\Events\RefundSuccessEvent' => [
-            'App\Listeners\UpdateSalesAndStockListener',# 更新库存+销量
-//            'App\Listeners\RefundSuccessListener'
+        'App\Events\RefundSuccessEvent' => [            # 更新退款数量
+            'App\Listeners\RefundSuccessListener'       # 退款成功返还库存 + 销量
+
         ],
+        'App\Events\PaySuccessEvent' => [               #  更新支付成功数量
+            'App\Listeners\NoticeListener'              #  消息通知
+        ],
+
     ];
 
     /**
